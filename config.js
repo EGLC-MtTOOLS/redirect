2beddd3559233a0b9bb737b6fe3a6de4d17dad72
EGLC-MtTOOLS <amongustav9@gmail.com>
2025-03-19 14:12:45 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Configuration settings for the redirect page
+export const config = {
+    // The URL to redirect to when conditions are met
+    redirectionLink: "https://vihaangayakwad.github.io/ClassroomBypass/",
+    
+    // Password protection settings
+    passwordEnabled: false,
+    password: "bobert!",
+    
+    // Site disabled settings
+    siteDisabled: false,
+    disabledRedirectionLink: "https://www.example.com"
+};
