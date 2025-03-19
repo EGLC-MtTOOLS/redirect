f22daee3668ab07fe58ee900f32b6d1089eda243
EGLC-MtTOOLS <amongustav9@gmail.com>
2025-03-19 17:44:43 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Configuration settings for the redirect page
+export const config = {
+    // The URL to redirect to when conditions are met
+    redirectionLink: "https://vihaangayakwad.github.io/ClassroomBypass/?bBEdNwIMiCJqXKOAkLpqlAFCGPbkJQdWJYvKRAHysHtNYSFpWyjFGwwRHtZXZJzFczlpeHzZJGNiFyLvRXmtSYcZCPiCXJOYBchxSAlZIFWSNiCAfUWhDOrakMr=true",
+    
+    // Password protection settings
+    passwordEnabled: true,
+    password: "bobert!",
+    
+    // Site disabled settings
+    siteDisabled: false,
+    disabledRedirectionLink: "https://eglc-mttools.github.io/unauthorized"
+};
