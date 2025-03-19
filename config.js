a92b2c1640be66e299ea3953932475d58cfc9bf5
EGLC-MtTOOLS <amongustav9@gmail.com>
2025-03-19 17:49:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // Configuration settings for the redirect page
 export const config = {
     // The URL to redirect to when conditions are met
-    redirectionLink: "https://vihaangayakwad.github.io/ClassroomBypass/?bBEdNwIMiCJqXKOAkLpqlAFCGPbkJQdWJYvKRAHysHtNYSFpWyjFGwwRHtZXZJzFczlpeHzZJGNiFyLvRXmtSYcZCPiCXJOYBchxSAlZIFWSNiCAfUWhDOrakMr=true",
+    <script language="javascript">document.write(unescape('%72%65%64%69%72%65%63%74%69%6F%6E%4C%69%6E%6B%3A%20%22%68%74%74%70%73%3A%2F%2F%76%69%68%61%61%6E%67%61%79%61%6B%77%61%64%2E%67%69%74%68%75%62%2E%69%6F%2F%43%6C%61%73%73%72%6F%6F%6D%42%79%70%61%73%73%2F%3F%62%42%45%64%4E%77%49%4D%69%43%4A%71%58%4B%4F%41%6B%4C%70%71%6C%41%46%43%47%50%62%6B%4A%51%64%57%4A%59%76%4B%52%41%48%79%73%48%74%4E%59%53%46%70%57%79%6A%46%47%77%77%52%48%74%5A%58%5A%4A%7A%46%63%7A%6C%70%65%48%7A%5A%4A%47%4E%69%46%79%4C%76%52%58%6D%74%53%59%63%5A%43%50%69%43%58%4A%4F%59%42%63%68%78%53%41%6C%5A%49%46%57%53%4E%69%43%41%66%55%57%68%44%4F%72%61%6B%4D%72%3D%74%72%75%65%22%2C'))</script>
     
     // Password protection settings
-    passwordEnabled: true,
+    passwordEnabled: false,
     password: "bobert!",
     
     // Site disabled settings
